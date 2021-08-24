016388a4c19bdd0b714664cb10ca0efadcd849e4
Cristian <cris.munoz0509@gmail.com>
2021-08-24 01:35:30 -0500

    htiangsdgasdf
    asdf

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-const APIController = (function () {
+const rates = document.getElementById("rates");
 
-    const clientID = '';
-    const clientSecret = '';
+const FetchRates = () => {
+    
+    const endpoint = 'live';
+    const url = 'http://api.coinlayer.com/' + endpoint + '?access_key=6bfe2cb9ec8df03ade6d71bfa7c0cfa1';
+    const reqData = fetch(url).then((res) => res.json())
+    DisplayRates(reqData)
+}
 
-    //private methods
-    const _getToken = async () => {
-        
-        const result = await fetch('https://accounts.spotify.com/api/token', {
-            method:'POST',
-            headers: {
-                'Content'
-            }
-        })
+const DisplayRates = (data) => {
+    const wantedRates = ['BTC', 'BAT', 'DGB'];
+    data.then(res => {
+        let htmlString;
+        for(let i = 0; i <= 2; i++){
+            htmlString = `
+                <li>
+                    <p>${wantedRates[i]}</p>
+                </li>
+            `
+            rates.innerHTML = htmlString;
+            console.log(wantedRates[i]);
+        }
+    });
+};
 
-    }
-
-})();
+FetchRates();
